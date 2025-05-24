adf26e06883e273bddd9123d9cad672f018b9dbc
GUSTAVO RIBEIRO DE LIMA <202503274797@alunos.unifanor.edu.br>
2025-05-23 21:41:16 -0300

    primeiro commit

---
+++ aula/teste01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<locale.h>
+
+int main () {
+     setlocale(LC_ALL,"Portuguese_Brazil.1252");
+
+
+     printf("Somos estudantes do curso de ADS da unifanor.Queremos estaR inseridos no mundo da tecnologia da informção.\n");
+
+return 0;
+}
